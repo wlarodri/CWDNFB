b25c30c93339b88392fdcf1a630290859ea44a9c
Wladimir Rodríguez <wlarodri@alumnos.ubiobio.cl>
2016-05-31 14:41:54 -0400

    Se agregó migración de la tabla peliculas

---
+++ database/migrations/2016_05_31_170433_create-pelicula_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePeliculaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('peliculas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('titulo');
+            $table->integer('año');
+            $table->string('genero');
+            $table->string('descripcion_pelicula');
+            $table->string('idiomas');
+            $table->string('imagen');
+            $table->string('director');
+            $table->string('web_oficial');
+            $table->string('actores');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('peliculas');
+    }
+}
