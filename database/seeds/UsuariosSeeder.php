39b4ca014c4351ae0174136961b452ce1a75adda
nicol <nbascuna@alumnos.ubiobio.cl>
2016-07-08 19:39:46 -0300

    New changelist

---
+++ database/seeds/UsuariosSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsuariosSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Usuario::create([
+            /*   'Nombre'=>'Nicol',
+                 'Apellido_Paterno'=>'Bascuñan',
+                 'Apellido_Materno'=>'Faúndez',
+                 'Fecha_Nacimiento'=>'20-07-1993',
+                 'ciudad'=>'concepcion',
+                 'genero'=>'femenino',
+                 'Nombre_usuario'=>'nicol.b',
+
+        ]);
+    }
+}
